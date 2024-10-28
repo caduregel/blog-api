f0319648354d31545cfd547a92889e3cc3325c2f
caduregel <liam@nehoray.net>
2024-10-28 17:59:28 +0100

    restarted project

--- routes/indexRouter.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { authRouter } from './authRouter.js';
-import { postsRouter } from './postsRouter.js';
-export const routes = express.Router();
-
-routes.use(authRouter);
-routes.use(postsRouter)
--- routes/postsRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createCommentController, createPostController, getPostController, getPostsController } from '../controllers/postsController.js';
-export const postsRouter = express.Router();
-
-// postsRouter.get('/posts/:id', getPostController)
-postsRouter.get('/posts', getPostsController)
-postsRouter.get('/posts/:id', getPostController)
-postsRouter.post('/posts/new-post', createPostController)
-postsRouter.post('/posts/:id/new-comment', createCommentController)
